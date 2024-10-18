15eab21f5365315aaaf49ce8c4d69524cec08e2f
JM (PC) <Nave@DESKTOP-U310PC0>
2024-10-18 13:41:43 +0800

    Add project files.

---
+++ Peak Performance Vehicle Rentals/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Peak_Performance_Vehicle_Rentals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
